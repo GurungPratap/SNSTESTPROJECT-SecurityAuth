04dbbdd8804d30c8111c6c5613dcb4e2286bab45
PratapGurung <prlg.dev52@gmail.com>
2022-02-26 15:18:30 -0600

    initial project setup for SNS test project Authorization server

---
+++ src/main/java/com/sns/core/secauth/SecAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sns.core.secauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecAuthApplication.class, args);
+    }
+
+}
